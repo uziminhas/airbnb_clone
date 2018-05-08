48fa2fb3dbc8bb282b7b952f2a00fc23347812ea
Uzair Minhas <uziminhas@gmail.com>
2018-05-07 22:18:35 -0400

    First commit

---
+++ src/screens/LoggedOut.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+
+export default class LoggedOut extends Component {
+  render() {
+    return <Text>HelloWorld</Text>;
+  }
+}
+
