6c3dd39342ea95d981c2ad5c7341d5e30d4cbc59
Uzair Minhas <uziminhas@gmail.com>
2018-05-07 22:53:44 -0400

    Adding logo

--- src/screens/LoggedOut.js
+++ src/screens/LoggedOut.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
+import colors from '../styles/colors';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
 } from 'react-native';
 
 
 export default class LoggedOut extends Component {
   render() {
-    return <Text>HelloWorld</Text>;
+    return (
+      <View style={styles.wrapper}>
+        <Image 
+          source={require('../img/airbnb-logo.png')}
+          style={styles.logo}
+        />
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+  	flex: 1,
+  	display: 'flex',
+  	backgroundColor: colors.green01,
+  },
+  logo: {
+  	width: 50,
+  	height: 50,
+  	marginTop: 50,
+  	marginBottom: 40,
+  }
+});
 
